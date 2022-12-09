fc491dcbb41bea79336e3ee86a550429d00ab49e
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-09 10:01:49 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
-* main- Entry
+* main- Entry point
 * Return- Always 0
 */
 
 int main(void)
-main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));

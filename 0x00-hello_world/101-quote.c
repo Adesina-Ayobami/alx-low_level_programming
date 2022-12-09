d596a4ad320aa0cbe0e6d6f716ba5ed1002e5845
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-09 10:17:58 -0800

    101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main- Entry point
-*  @void: parameter
-* Return: 1 (success)
+* main- Entry
+* @void- perimeter
+* Return- Always 1
 */
 
 int main(void)
 {
-	printf(stderr, "and that piece of art is useful\" - \
-	Dora Korpar, 2015-10-19\n");
+	printf(stderr, "and that piece of art is useful\" - \Dora Korpar, 2015-10-19\n");
 	return (1);
 }

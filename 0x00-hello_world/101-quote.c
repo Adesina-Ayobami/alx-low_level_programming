533cfe6119fc840a220d2bd49f58cd1abbc03e0e
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-10 23:48:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a C program
- * Code by Adesina Ayobami
+ * main - entry point
+ *
  * Return: 1
 */
 int main(void)
 {
-	fprintf("and that piece of art is useful\"-\ Dora Korpar,2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

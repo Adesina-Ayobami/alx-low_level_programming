4694ddbe5e8398987959d5c0deb6300cac3b969f
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-10 22:12:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int7 main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	printf("\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

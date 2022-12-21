9cc242da05a07a8e3afd3356fab9d9294094c249
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-20 22:01:57 -0800

    2mul is done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * mul - multiplying two integers
+ * mul -> multiplying two integers
  * @a: first integer
  * @b: second integer
  * Return: additon of a and b
  */
+
 int mul(int a, int b)
 {
 	return (a * b);

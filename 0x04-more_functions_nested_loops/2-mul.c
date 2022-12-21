ac4e9880b3e7b3d4fe03632567c9c0f3a3567d75
Adesina Ayobami <Adesinaayobamidele3@gmail.com>
2022-12-20 21:50:35 -0800

    multiplication 2 is done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul -. multiplying 2 numbers
+ @a: interger param1
+ @b: integer param 2
+Return: addition of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
